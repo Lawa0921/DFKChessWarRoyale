d38c200ee676c4b3e0b240a53ebac5a6cb738bea
Galen Lin <bag571ivy3470@gmail.com>
2022-09-23 16:20:56 +0800

    chore(prefab): adjust hexes button component nav logic

--- Assets/Scripts/FieldManager.cs
+++ Assets/Scripts/FieldManager.cs
@@ -15,6 +15,7 @@ public class FieldManager : MonoBehaviour
             allRows[i].allHexesInRow = allRows[i].GetComponentsInChildren<BattleHex>();
         }
         CreateAllHexesArray();
+        CreateActiveHexesArray();
     }
 
     private void CreateAllHexesArray()
@@ -30,6 +31,21 @@ public class FieldManager : MonoBehaviour
                 allHexesArray[j, i] = allRows[i].allHexesInRow[widthOfArray - j - 1];
                 allHexesArray[j, i].verticalCoordinate = heightOfArray - i;
                 allHexesArray[j, i].horizontalCoordinate = widthOfArray - j;
+            }
+        }
+    }
+
+    private void CreateActiveHexesArray()
+    {
+        foreach (BattleHex hex in allHexesArray)
+        {
+            if (Mathf.Abs(hex.transform.position.x) > 8.5f || Mathf.Abs(hex.transform.position.y) > 4.5f )
+            {
+                hex.SetInActive();
+            }
+            else
+            {
+                hex.SetActive();
             }
         }
     }
