b7b051e678e701275c4d60186cc2b7f6489472ae
Galen Lin <bag571ivy3470@gmail.com>
2022-10-18 14:17:54 +0800

    chore(project): reset project and add tile resource

---
+++ Assets/Scripts/Cell/Base.cs
@@ -0,0 +1,12 @@
+﻿using TbsFramework.Cells;
+using UnityEngine;
+
+public class Base : Square
+{
+    Vector3 dimensions = new Vector3(1.28f, 1.28f, 0f);
+
+    public override Vector3 GetCellDimensions()
+    {
+        return dimensions;
+    }
+}
