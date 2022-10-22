4c34e4a20fe2160f9a031cc873eb5d09e8e04723
Galen Lin <bag571ivy3470@gmail.com>
2022-10-22 17:03:11 +0800

    chore(game): adjust game template

---
+++ Assets/Scripts/SampleUnit.cs
@@ -0,0 +1,39 @@
+using TbsFramework.Units;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleUnit : Unit
+{
+    public Color LeadingColor;
+    public override void Initialize()
+    {
+        base.Initialize();
+        GetComponentInChildren<Renderer>().material.color = LeadingColor;
+    }
+
+    public override void MarkAsFriendly()
+    {
+        GetComponentInChildren<Renderer>().material.color = new Color(0.8f, 1, 0.8f);
+    }
+
+    public override void MarkAsReachableEnemy()
+    {
+        GetComponentInChildren<Renderer>().material.color = Color.red;
+    }
+
+    public override void MarkAsSelected()
+    {
+        GetComponentInChildren<Renderer>().material.color = Color.green;
+    }
+
+    public override void MarkAsFinished()
+    {
+        GetComponentInChildren<Renderer>().material.color = Color.gray;
+    }
+
+    public override void UnMark()
+    {
+        GetComponentInChildren<Renderer>().material.color = LeadingColor;
+    }
+}
