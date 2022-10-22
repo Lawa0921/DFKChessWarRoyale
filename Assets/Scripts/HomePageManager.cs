66a345d03735840f06dfaa2c99226ecf6a7fa9b2
Galen Lin <bag571ivy3470@gmail.com>
2022-10-22 18:16:24 +0800

    chore(network): import PUN and update homepage

---
+++ Assets/Scripts/HomePageManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class HomePageManager : MonoBehaviour
+{
+    public void OnClickLogin()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+        print("clicked login");
+    }
+
+    public void OnClickSignUp()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+        print("clicked sign up");
+    }
+}
